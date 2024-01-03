e126356343856659e5f334276ea6d6bad2b6c65c
ChobitsM01 <chobitsm01@gmail.com>
2024-01-03 22:30:24 +0700

    enable, disable guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './decorator/customize';
 
 @Controller() //  route /
 export class AppController {
@@ -13,15 +14,23 @@ export class AppController {
     private authService: AuthService
   ) { }
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
     return this.authService.login(req.user);
   }
 
+  @Public()
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile1')
+  getProfile1(@Request() req) {
     return req.user;
   }
 }
